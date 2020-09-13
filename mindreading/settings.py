6a51ef678d64f212ed0d56b2c5d199fb2d7299a6
Eunjin Lee <leee5495@gmail.com>
2020-09-14 01:47:08 +0900

    fix mindreading/ directory

--- mindreading/settings.py
+++ mindreading/settings.py
@@ -72,39 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mindreading.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-if os.getenv('GAE_APPLICATION', None):
-    # Running on production App Engine, so connect to Google Cloud SQL using
-    # the unix socket at /cloudsql/<your-cloudsql-connection string>
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '/cloudsql/mindreading:asia-northeast2:mindreadingdb-2019',
-            'USER': 'root',
-            'PASSWORD': 'mindreading2019',
-            'NAME': 'mindreading_db',
-        }
-    }
-else:
-    # Running locally so connect to either a local MySQL instance or connect to
-    # Cloud SQL via the proxy. To start the proxy via command line:
-    #
-    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
-    #
-    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '127.0.0.1',
-            'PORT': '3307',
-            'NAME': 'mindreading_db',
-            'USER': 'root',
-            'PASSWORD': 'mindreading2019',
-        }
-    }
-
-"""
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql',
@@ -115,7 +82,7 @@ DATABASES = {
        'PASSWORD': '970909',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- mindreading/urls.py
+++ mindreading/urls.py
@@ -23,4 +23,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('info/', include('info.urls')),
 ]
-
